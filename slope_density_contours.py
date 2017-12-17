56522367b38f245f124d70ac197a7d59d6711ce4
Bhavin Joshi <bajoshi@asu.edu>
2017-12-17 14:21:22 +0530

    updated code to make contours look better

--- slope_density_contours.py
+++ slope_density_contours.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import numpy as np
+from astropy.convolution import convolve, Gaussian2DKernel
 
 import sys
 import os
@@ -9,13 +10,17 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, AnchoredText
+import matplotlib as mpl
 
 home = os.getenv('HOME')
 slopedir = home + '/Desktop/slope-effects-density/'
 slope_extdir = home + '/Documents/plots_codes_for_heather/slope_effects_files/'
+taffy_dir = home + '/Desktop/ipac/taffy/'
 
 sys.path.append(slopedir)
+sys.path.append(taffy_dir + 'codes/')
 import plot_maps as pm
+import vel_channel_map as vcm
 
 def make_plot_diam_bin_with_contour(density_arr_color, slope_arr_color, color_arr, diam_bin_min, diam_bin_max):
 
@@ -33,7 +38,7 @@ def make_plot_diam_bin_with_contour(density_arr_color, slope_arr_color, color_ar
     x = slope_arr_color[diam_bin_idx]
     y = density_arr_color[diam_bin_idx]
 
-    ax.scatter(x, np.log10(y), s=5, c=color_arr[diam_bin_idx], alpha=0.4, edgecolors='none')
+    ax.scatter(x, np.log10(y), s=5, c=color_arr[diam_bin_idx], alpha=0.3, edgecolors='none')
     ax.set_ylim(-8, 0.5)
     ax.set_xlim(0, 35)
 
@@ -47,14 +52,21 @@ def make_plot_diam_bin_with_contour(density_arr_color, slope_arr_color, color_ar
     yp = y[fin_idx]
 
     counts, xbins, ybins = np.histogram2d(xp, np.log10(yp), bins=25, normed=False)
+    # smooth counts to get smoother contours
+    kernel = Gaussian2DKernel(stddev=1.4)
+    counts = convolve(counts, kernel, boundary='extend')
+
     print np.min(counts), np.max(counts)
-    levels_to_plot, cb_lw = get_levels_to_plot(diam_bin)
+    levels_to_plot, cb_lw, vmin = get_levels_to_plot(diam_bin)
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=max(levels_to_plot))
+
     c = ax.contour(counts.transpose(), levels=levels_to_plot, \
         extent=[xbins.min(), xbins.max(), ybins.min(), ybins.max()], \
-        cmap=cm.copper, linestyles='solid', linewidths=2, interpolation='None', zorder=10)
+        cmap=cm.viridis, linestyles='solid', linewidths=2, \
+        interpolation='None', zorder=10, norm=norm)
 
     # plot colorbar inside figure
-    cbaxes = inset_axes(ax, width='3%', height='50%', loc=7, bbox_to_anchor=[-0.05, -0.2, 1, 1], bbox_transform=ax.transAxes)
+    cbaxes = inset_axes(ax, width='3%', height='52%', loc=7, bbox_to_anchor=[-0.05, -0.2, 1, 1], bbox_transform=ax.transAxes)
     cb = plt.colorbar(c, cax=cbaxes, ticks=[min(levels_to_plot), max(levels_to_plot)], orientation='vertical')
     cb.ax.get_children()[0].set_linewidths(cb_lw)
 
@@ -82,49 +94,63 @@ def make_plot_diam_bin_with_contour(density_arr_color, slope_arr_color, color_ar
 def get_levels_to_plot(diam_bin):
 
     if diam_bin == '1to2':
-        levels_to_plot = [10, 50, 200, 500, 675]
-        cb_lw = 34.0
+        levels_to_plot = [10, 100, 200, 335, 450]
+        cb_lw = 35.0
+        vmin = -150
     elif diam_bin == '2to3':
-        levels_to_plot = [3, 10, 50, 100, 200]
-        cb_lw = 34.0
+        levels_to_plot = [3, 20, 50, 80, 120]
+        cb_lw = 35.0
+        vmin = -40
     elif diam_bin == '3to4':
-        levels_to_plot = [3, 10, 25, 50, 120]
-        cb_lw = 34.0
+        levels_to_plot = [3, 15, 30, 50, 68]
+        cb_lw = 35.0
+        vmin = -20
     elif diam_bin == '4to5':
-        levels_to_plot = [3, 12, 25, 43, 58]
-        cb_lw = 34.0
+        levels_to_plot = [3, 10, 15, 24, 34]
+        cb_lw = 35.0
+        vmin = -8
     elif diam_bin == '5to6':
-        levels_to_plot = [2, 10, 20, 30, 70]
-        cb_lw = 34.0
+        levels_to_plot = [2, 10, 18, 25, 37]
+        cb_lw = 35.0
+        vmin = -10
     elif diam_bin == '6to7':
-        levels_to_plot = [1.9, 10, 20, 30, 70]
-        cb_lw = 34.0
+        levels_to_plot = [2, 10, 18, 25, 37]
+        cb_lw = 35.0
+        vmin = -10
     elif diam_bin == '7to8':
-        levels_to_plot = [2, 8, 20, 35, 50]
-        cb_lw = 34.0
+        levels_to_plot = [2, 8, 13, 18, 24]
+        cb_lw = 35.0
+        vmin = -5
     elif diam_bin == '8to9':
-        levels_to_plot = [2, 8, 20, 35, 50]
-        cb_lw = 34.0
+        levels_to_plot = [2, 8, 13, 18, 23]
+        cb_lw = 35.0
+        vmin = -5
     elif diam_bin == '9to10':
-        levels_to_plot = [2, 6, 10, 20, 30]
-        cb_lw = 34.0
+        levels_to_plot = [1, 4, 7, 9, 12]
+        cb_lw = 35.0
+        vmin = -2
     elif diam_bin == '10to15':
-        levels_to_plot = [5, 10, 20, 50, 100, 175]
-        cb_lw = 27.0
+        levels_to_plot = [5, 20, 40, 65, 83, 95]
+        cb_lw = 28.0
+        vmin = -20
     elif diam_bin == '15to20':
-        levels_to_plot = [3, 10, 20, 50, 100, 150]
-        cb_lw = 27.0
+        levels_to_plot = [3, 12, 30, 50, 68, 80]
+        cb_lw = 28.0
+        vmin = -20
     elif diam_bin == '20to25':
-        levels_to_plot = [5, 20, 50, 100, 300, 550]
-        cb_lw = 27.0
+        levels_to_plot = [5, 50, 140, 200, 250, 280]
+        cb_lw = 28.0
+        vmin = -50
     elif diam_bin == '25to30':
-        levels_to_plot = [5, 20, 50, 100, 250, 600]
-        cb_lw = 27.0
+        levels_to_plot = [5, 50, 140, 200, 250, 280]
+        cb_lw = 28.0
+        vmin = -50
     elif diam_bin == '30to35':
-        levels_to_plot = [5, 20, 50, 100, 250, 600]
-        cb_lw = 27.0
+        levels_to_plot = [5, 50, 140, 200, 250, 280]
+        cb_lw = 28.0
+        vmin = -60
 
-    return levels_to_plot, cb_lw
+    return levels_to_plot, cb_lw, vmin
 
 if __name__ == '__main__':
 
