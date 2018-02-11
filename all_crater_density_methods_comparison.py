6b2e32b211b7e92dcc94896237af64b75ce3008f
Bhavin Joshi <bajoshi@asu.edu>
2018-02-11 13:06:45 -0700

    added code for comparing all crater density methods

---
+++ all_crater_density_methods_comparison.py
@@ -0,0 +1,27 @@
+from __future__ import division
+
+import numpy as np
+
+import sys
+import os
+
+import matplotlib.pyplot as plt
+
+home = os.getenv('HOME')
+slopedir = home + '/Desktop/slope-effects-density/'
+slope_extdir = home + '/Documents/plots_codes_for_heather/slope_effects_files/'
+
+if __name__ == '__main__':
+	
+    # read in all density and slope rasters
+    # and put them in numpy arrays.
+    point_density = 
+    slope_pointdensity = 
+
+    boolean_density = 
+    slope_booleandensity = 
+
+    fuzzy_density = 
+    slope_fuzzydensity = 
+
+	sys.exit(0)
