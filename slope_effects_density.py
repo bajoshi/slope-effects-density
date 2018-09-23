Fixed new boolean method computation

slope_effects_density.py
@@ -1079,27 +1079,25 @@ if __name__ == '__main__':
     # the endpoint is NOT included. E.g. 1-2 includes 1 <= D < 2
     # I've set the dtype to float32 to save space
     crater_frac_diambin_1_1p25   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_1p25_1p5   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_1p5_1p75   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_1p25_1p5 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_1p5_1p75 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_1p75_2   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
 
     crater_frac_diambin_2_2p25   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_2p25_2p5   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_2p5_2p75   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_2p25_2p5 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_2p5_2p75 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_2p75_3   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
 
     crater_frac_diambin_3_3p25   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_3p25_3p5   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_3p5_3p75   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_3p25_3p5 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_3p5_3p75 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_3p75_4   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
 
     crater_frac_diambin_4_4p25   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_4p25_4p5   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-    crater_frac_diambin_4p5_4p75   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_4p25_4p5 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_4p5_4p75 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_4p75_5   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
-
     # ---
-
     crater_frac_diambin_5_6   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_6_7   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_7_8   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
@@ -1110,6 +1108,38 @@ if __name__ == '__main__':
     crater_frac_diambin_20_25 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_25_30 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
     crater_frac_diambin_30_35 = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+
+    # ----------------------- Empty arrays for new boolean method ----------------------- #
+    crater_frac_diambin_1_1p25_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_1p25_1p5_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_1p5_1p75_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_1p75_2_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+
+    crater_frac_diambin_2_2p25_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_2p25_2p5_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_2p5_2p75_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_2p75_3_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+
+    crater_frac_diambin_3_3p25_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_3p25_3p5_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_3p5_3p75_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_3p75_4_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+
+    crater_frac_diambin_4_4p25_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_4p25_4p5_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_4p5_4p75_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_4p75_5_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    # ---
+    crater_frac_diambin_5_6_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_6_7_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_7_8_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_8_9_newbool   = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_9_10_newbool  = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_10_15_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_15_20_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_20_25_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_25_30_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
+    crater_frac_diambin_30_35_newbool = np.zeros(len(pix_x_cen_arr), dtype=np.float32)
 
     # also create a blank array for associating 
     # crater ids with each pixel. I need a blank 
@@ -1201,8 +1231,35 @@ if __name__ == '__main__':
 
             # find the area of intersection between the pixel and crater and
             # the fraction of original crater that area amounts to
-            inter_area = (pixel_corners & crater_poly).area()
+            inter_area = (pixel_corners & crater_poly).area()  # in square meters
             inter_area_crater_frac = inter_area / crater_poly.area() # store the fraction of the crater occupying that pixel
+            # Both the areas are in square meters. So the fraction will be correct.
+            # You can check this simply by doing:
+            # print pixel_corners.area()
+            # print crater_poly.area()
+            # These will come out in sq.meters.
+            # i.e. the areas are in sq.meters because the pixel centers 
+            # and the crater vertices have been provided in meters.
+
+            # -------------------------------------- New Boolean method -------------------------------------- #
+            """
+            The new boolean method simply weights the larger craters the same as the smaller craters.
+            We realized that computing fractional areas of craters within pixels would (incorrectly)
+            up-weight the smaller craters since their fractions would be much closer to 1 relative to 
+            the larger craters simply because of their size. This would mean even if the larger craters
+            covered the entire area of a pixel the fractional crater area would never be as large as 
+            that for a relatively smaller crater. Therefore, we would automatically never get large 
+            density values for large craters.
+
+            To offset this effect we have decided to use the "New Boolean Method" which effectively
+            weights the larger craters the same as the smaller craters. With this method we assign
+            a crater_fractional_area value of 1 to every pixel that has 50% or larger of its area
+            covered by craters. 
+            """
+            # ------- Find fraction of pixel occupied by crater ------- #
+            pix_frac_occ_crater = inter_area / pixel_corners.area()
+            if pix_frac_occ_crater >= 0.5:
+                inter_area_crater_frac_newbool = 1.0
 
             # find pixel index using pixel center to append to the correct array element
             pix_index = pixel_indices[j]
@@ -1223,62 +1280,91 @@ if __name__ == '__main__':
             # This should still be a cumulative sum within each diamter bin
             if current_diam >= 1 and current_diam < 1.25:
                 crater_frac_diambin_1_1p25[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_1_1p25_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 1.25 and current_diam < 1.5:
                 crater_frac_diambin_1p25_1p5[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_1p25_1p5_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 1.5 and current_diam < 1.75:
                 crater_frac_diambin_1p5_1p75[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_1p5_1p75_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 1.75 and current_diam < 2:
                 crater_frac_diambin_1p75_2[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_1p75_2_newbool[pix_index] += inter_area_crater_frac_newbool
+
 
             elif current_diam >= 2 and current_diam < 2.25:
                 crater_frac_diambin_2_2p25[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_2_2p25_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 2.25 and current_diam < 2.5:
                 crater_frac_diambin_2p25_2p5[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_2p25_2p5_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 2.5 and current_diam < 2.75:
                 crater_frac_diambin_2p5_2p75[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_2p5_2p75_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 2.75 and current_diam < 3:
                 crater_frac_diambin_2p75_3[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_2p75_3_newbool[pix_index] += inter_area_crater_frac_newbool
+
 
             elif current_diam >= 3 and current_diam < 3.25:
                 crater_frac_diambin_3_3p25[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_3_3p25_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 3.25 and current_diam < 3.5:
                 crater_frac_diambin_3p25_3p5[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_3p25_3p5_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 3.5 and current_diam < 3.75:
                 crater_frac_diambin_3p5_3p75[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_3p5_3p75_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 3.75 and current_diam < 4:
                 crater_frac_diambin_3p75_4[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_3p75_4_newbool[pix_index] += inter_area_crater_frac_newbool
+
 
             elif current_diam >= 4 and current_diam < 4.25:
                 crater_frac_diambin_4_4p25[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_4_4p25_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 4.25 and current_diam < 4.5:
                 crater_frac_diambin_4p25_4p5[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_4p25_4p5_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 4.5 and current_diam < 4.75:
                 crater_frac_diambin_4p5_4p75[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_4p5_4p75_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 4.75 and current_diam < 5:
                 crater_frac_diambin_4p75_5[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_4p75_5_newbool[pix_index] += inter_area_crater_frac_newbool                
 
             # ---
 
             elif current_diam >= 5 and current_diam < 6:
                 crater_frac_diambin_5_6[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_5_6_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 6 and current_diam < 7:
                 crater_frac_diambin_6_7[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_6_7_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 7 and current_diam < 8:
                 crater_frac_diambin_7_8[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_7_8_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 8 and current_diam < 9:
                 crater_frac_diambin_8_9[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_8_9_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 9 and current_diam < 10:
                 crater_frac_diambin_9_10[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_9_10_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 10 and current_diam < 15:
                 crater_frac_diambin_10_15[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_10_15_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 15 and current_diam < 20:
                 crater_frac_diambin_15_20[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_15_20_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 20 and current_diam < 25:
                 crater_frac_diambin_20_25[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_20_25_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 25 and current_diam < 30:
                 crater_frac_diambin_25_30[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_25_30_newbool[pix_index] += inter_area_crater_frac_newbool
             elif current_diam >= 30 and current_diam < 35:
                 crater_frac_diambin_30_35[pix_index] += inter_area_crater_frac
+                crater_frac_diambin_30_35_newbool[pix_index] += inter_area_crater_frac_newbool
 
     # pix_crater_area /= 1e6 -- normalized to 1 sq km if needed (comment out if using fractions)
 
@@ -1307,6 +1393,15 @@ if __name__ == '__main__':
     crater_frac_diambin_9_10, crater_frac_diambin_10_15, crater_frac_diambin_15_20, crater_frac_diambin_20_25, \
     crater_frac_diambin_25_30, crater_frac_diambin_30_35]
 
+    all_crater_fractions_newbool = [crater_frac_diambin_1_1p25_newbool, crater_frac_diambin_1p25_1p5_newbool, \
+    crater_frac_diambin_1p5_1p75_newbool, crater_frac_diambin_1p75_2_newbool, \
+    crater_frac_diambin_2_2p25_newbool, crater_frac_diambin_2p25_2p5_newbool, crater_frac_diambin_2p5_2p75_newbool, crater_frac_diambin_2p75_3_newbool, \
+    crater_frac_diambin_3_3p25_newbool, crater_frac_diambin_3p25_3p5_newbool, crater_frac_diambin_3p5_3p75_newbool, crater_frac_diambin_3p75_4_newbool, \
+    crater_frac_diambin_4_4p25_newbool, crater_frac_diambin_4p25_4p5_newbool, crater_frac_diambin_4p5_4p75_newbool, crater_frac_diambin_4p75_5_newbool, \
+    crater_frac_diambin_5_6_newbool, crater_frac_diambin_6_7_newbool, crater_frac_diambin_7_8_newbool, crater_frac_diambin_8_9_newbool, \
+    crater_frac_diambin_9_10_newbool, crater_frac_diambin_10_15_newbool, crater_frac_diambin_15_20_newbool, crater_frac_diambin_20_25_newbool, \
+    crater_frac_diambin_25_30_newbool, crater_frac_diambin_30_35_newbool]
+
     all_crater_fractions_names = ['crater_frac_diambin_1_1p25', 'crater_frac_diambin_1p25_1p5', 'crater_frac_diambin_1p5_1p75', \
     'crater_frac_diambin_1p75_2', \
     'crater_frac_diambin_2_2p25', 'crater_frac_diambin_2p25_2p5', 'crater_frac_diambin_2p5_2p75', 'crater_frac_diambin_2p75_3', \
@@ -1318,6 +1413,7 @@ if __name__ == '__main__':
 
     for v in range(len(all_crater_fractions)):
         np.save(slope_extdir + all_crater_fractions_names[v] + '.npy', all_crater_fractions[v])
+        np.save(slope_extdir + all_crater_fractions_names[v] + '_newbool.npy', all_crater_fractions_newbool[v])
 
     # save as numpy binary array and csv
     np.save(slope_extdir + 'crater_area_frac_in_pix_fastcomp.npy', pix_crater_area)
@@ -1328,36 +1424,6 @@ if __name__ == '__main__':
 
     # save as ascii raster
     #su.numpy_to_asciiraster(slope_extdir + 'crater_area_frac_in_pix_fastcomp.npy', (rows, columns), pix_x_cen_arr, pix_y_cen_arr)
-
-    # -------------------------------------- New Boolean method -------------------------------------- #
-    """
-    The new boolean method simply weights the larger craters the same as the smaller craters.
-    We realized that computing fractional areas of craters within pixels would (incorrectly)
-    up-weight the smaller craters since their fractions would be much closer to 1 relative to 
-    the larger craters simply because of their size. This would mean even if the larger craters
-    covered the entire area of a pixel the fractional crater area would never be as large as 
-    that for a relatively smaller crater. Therefore, we would automatically never get large 
-    density values for large craters.
-
-    To offset this effect we have decided to use the "New Boolean Method" which effectively
-    weights the larger craters the same as the smaller craters. With this method we assign
-    a crater_fractional_area value of 1 to every pixel that has 50% or larger of its area
-    covered by craters. 
-    """
-    use_boolean_new = True
-
-    if use_boolean_new == True:
-        print "Computing and saving crater fraction for new boolean method now."
-
-        for w in range(len(all_crater_fractions)):
-
-            current_crater_frac_list = all_crater_fractions[w]
-
-            bool_crater_frac_idx = np.where(current_crater_frac_list >= 0.5)
-            current_crater_frac_list[bool_crater_frac_idx] = 1.0
-
-            # Save results from new boolean method
-            np.save(slope_extdir + all_crater_fractions_names[w] + '_newbool.npy', current_crater_frac_list)
 
     print "\n","Crater fractional area in each pixel computation done and saved."
 